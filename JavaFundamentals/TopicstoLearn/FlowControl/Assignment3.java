e1ba31b7beed4d2d7d859075b0238aeab430a15c
meghanamathi <meghanamathi10@gmail.com>
2026-07-03 19:10:20 +0530

    Completed assignment3 on FlowControl

---
+++ JavaFundamentals/TopicstoLearn/FlowControl/Assignment3.java
@@ -0,0 +1,17 @@
+package JavaFundamentals.TopicstoLearn.FlowControl;
+
+public class Assignment3 {
+    public static void main(String[] args) {
+
+        if(args.length==0)
+            System.out.println("No Values");
+        else{
+            for(int i=0;i<args.length;i++){
+                if(i!=args.length-1)
+                    System.out.print(args[i]+",");
+                else
+                    System.out.print(args[i]);
+            }
+        }
+    }
+}
