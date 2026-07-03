c9dc3a9cdb4af57528426092daf40aa93a6c8e82
meghanamathi <meghanamathi10@gmail.com>
2026-07-03 19:08:00 +0530

    Completed FlowControl Assignment2

---
+++ JavaFundamentals/TopicstoLearn/FlowControl/Assignment2.java
@@ -0,0 +1,19 @@
+package JavaFundamentals.TopicstoLearn.FlowControl;
+
+import java.util.Scanner;
+
+public class Assignment2 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+
+        sc.close();
+    }
+}
