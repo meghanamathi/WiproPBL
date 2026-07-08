2b40a447ded68f37d3f1f9b11cbd6543a973c62d
meghanamathi <meghanamathi10@gmail.com>
2026-07-08 10:36:20 +0530

    Completed Assignment 1 and 2 of Oops -Polymorphism/Overriding

---
+++ Oops/TopicstoLearn/Polymorphism/Assignment1.java
@@ -0,0 +1,54 @@
+package Oops.TopicstoLearn.Polymorphism;
+
+class Fruit {
+
+    String name;
+    String taste;
+    String size;
+
+    Fruit(String name, String taste, String size) {
+        this.name = name;
+        this.taste = taste;
+        this.size = size;
+    }
+
+    public void eat() {
+        System.out.println(name + " tastes " + taste);
+    }
+}
+
+class Apple extends Fruit {
+
+    Apple() {
+        super("Apple", "Sweet", "Medium");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Apple tastes Sweet");
+    }
+}
+
+class Orange extends Fruit {
+
+    Orange() {
+        super("Orange", "Sour", "Medium");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Orange tastes Sour");
+    }
+}
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+
+        Apple apple = new Apple();
+        Orange orange = new Orange();
+
+        apple.eat();
+        orange.eat();
+    }
+}
